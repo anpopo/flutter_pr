e191b20e5e6fa5cb53400e3bb3229f012fa28198
ansehyeong <dkstpgud@gmail.com>
2023-09-05 21:04:32 +0900

    [testing_app] feat: unit testing the provider

---
+++ lib/testing_app/testing_app_main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:first_web/testing_app/models/favorites.dart';
+import 'package:first_web/testing_app/screens/favorites.dart';
+import 'package:first_web/testing_app/screens/home.dart';
+
+void main() {
+  runApp(const TestingApp());
+}
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: HomePage.routeName,
+      builder: (context, state) {
+        return const HomePage();
+      },
+      routes: [
+        GoRoute(
+          path: FavoritesPage.routeName,
+          builder: (context, state) {
+            return const FavoritesPage();
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
+class TestingApp extends StatelessWidget {
+  const TestingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp.router(
+        title: 'Testing Sample',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: true,
+        ),
+        routerConfig: _router,
+      ),
+    );
+  }
+}
